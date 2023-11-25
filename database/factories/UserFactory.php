22e043ee87c8a766ca349a04fca5af07474dedfb
Balazs Karacsony <karacsonybalazs01@gmail.com>
2023-11-25 17:58:07 +0100

    Edited UserFactory to match username with email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,11 +18,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $Username = fake()->firstName()." ".fake()->lastName();
+        $UsernameParts = explode(" ", $Username);
+        $Domains = ["@gmail.com", "@protonmail.com", "@ncob.com", "@simgatsy.com", "@simgatla.com", "@flipbasket.tp"];
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $Username,
+            'email' => strtolower($UsernameParts[0]).".".strtolower($UsernameParts[1]).fake()->randomElement($Domains),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'), // password
+            'profile_picture' => "https://ui-avatars.com/api/?size=256&background=random&name={$UsernameParts[0]}+{$UsernameParts[1]}",
             'remember_token' => Str::random(10),
         ];
     }
