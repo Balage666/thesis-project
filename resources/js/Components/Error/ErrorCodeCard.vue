f25444781abf66ddff1cd1754973e6935935fa79
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-01 14:28:38 +0100

    Created component for displaying html error codes

---
+++ resources/js/Components/Error/ErrorCodeCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+import { router } from '@inertiajs/vue3';
+import { onUnmounted, onBeforeUnmount, onMounted } from 'vue'
+
+const props = defineProps({
+    statusCode: Number,
+    title: String,
+    description: String
+})
+
+onMounted(() => {
+    setTimeout(() => {
+        router.get('/storefront')
+    }, 2500)
+})
+
+</script>
+
+
+
+<template>
+    <Head>
+        <title>{{ props.title }}</title>
+    </Head>
+    <div class="container vh-100 d-flex flex-grow-1 justify-content-center align-items-center">
+        <div class="card bg-light-subtle vw-100" style="height: 24rem">
+            <div class="d-flex align-items-center justify-content-center vh-100">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold">{{ props.statusCode }}</h1>
+                    <p class="fs-3"> {{ props.title }}</p>
+                    <p class="lead">
+                        {{ props.description }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
