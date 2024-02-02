7d7639f844a2b1deb40ce5417c3069a412cb032c
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-02 15:15:15 +0100

    Added ziggy to vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,12 +8,15 @@ import '../css/app.css'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from 'ziggy-js';
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        const VueApp = createApp({ render: () => h(App, props) })
         .use(plugin)
-        .mount(el)
+        .use(ZiggyVue, Ziggy);
+
+        VueApp.mount(el);
     },
 });
