fd25094920f2d89a4f37a610d28153e447bc462e
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-02 15:13:01 +0100

    Added ziggy to project

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            'ziggy-js': path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
         }
     },
 });
