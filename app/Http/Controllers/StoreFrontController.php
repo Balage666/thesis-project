f70475a235d6b767c3d2266e1fd8687d5c7d4721
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-02 18:01:18 +0100

    Added new line for debugging
    Wil be deleted later

--- app/Http/Controllers/StoreFrontController.php
+++ app/Http/Controllers/StoreFrontController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Inertia\Inertia;
 
 class StoreFrontController extends Controller
 {
     public function StoreFront() {
 
+
+        // dd(User::find(1)->id);
         // return view('storefront.welcome');
         return Inertia::render("Home");
     }
