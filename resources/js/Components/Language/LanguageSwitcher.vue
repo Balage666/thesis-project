68b2389a359f1ee970e50d3321aa707c055a6858
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-04 17:06:47 +0100

    Created a minimal lang switcher comp.

---
+++ resources/js/Components/Language/LanguageSwitcher.vue
@@ -0,0 +1,20 @@
+<template>
+    <select @change="switchLanguage" class="form-select form-select-lg" name="language_switcher" id="language_switcher">
+        <option v-for="(lang, idx) in languages" :value="idx">{{ __(lang) }}</option>
+    </select>
+</template>
+
+<script setup>
+
+import { reactive, computed } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
+
+const page = usePage();
+
+const languages = computed(() => page.props.value.locales);
+
+// console.log(languages.value);
+
+const switchLanguage = ($event) => console.log($event.value)
+
+</script>
