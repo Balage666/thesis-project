e001405b11ce61f66185f7fdccbb889ef2cc88f4
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-06 12:04:51 +0100

    Added formkit/vue comps.  and tried it out

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,14 +1,88 @@
 <script setup>
 
 import { Link } from '@inertiajs/inertia-vue3'
+import { ref } from 'vue';
+import { FormKit } from '@formkit/vue';
+
 
 import LanguageSwitcher from '../Components/Language/LanguageSwitcher.vue';
 
+//TODO: Remove commented lines
+// const step = ref('contactInfo')
+// const stepNames = ['contactInfo', 'organizationInfo', 'application']
+
+// const camel2title = (str) =>
+//     str
+//     .replace(/([A-Z])/g, (match) => ` ${match}`)
+//     .replace(/^./, (match) => match.toUpperCase())
+//     .trim()
+
+
 </script>
+
+<!--<style scoped>
+
+@import 'https://cdn.formk.it/web-assets/multistep-form.css';
+
+</style>-->
 
 <template>
     <div>
-        <h1>Vue3 test</h1>
+        <h1>Vue3 & FormKit test</h1>
+
+
+        <!-- <h1 class="text-2xl font-bold mb-4">Carbon Sequestration Grant</h1>
+
+        <FormKit type="form" #default="{ value }">
+            <ul class="steps">
+            <li
+                v-for="stepName in stepNames"
+                class="step"
+                @click="step = stepName"
+                :data-step-active="step === stepName"
+            >
+                {{ camel2title(stepName) }}
+            </li>
+            </ul>
+
+            <div class="form-body">
+            <section v-show="step === 'contactInfo'">
+                <FormKit type="group" id="contactInfo" name="contactInfo">
+                <FormKit
+                    type="email"
+                    label="*Email address"
+                    validation="required|email"
+                />
+                </FormKit>
+            </section>
+
+            <section v-show="step === 'organizationInfo'">
+                <FormKit id="organizationInfo" type="group" name="organizationInfo">
+                <FormKit
+                    type="text"
+                    label="*Organization name"
+                    validation="required|length:3"
+                />
+                </FormKit>
+            </section>
+
+            <section v-show="step === 'application'">
+                <FormKit id="application" type="group" name="application">
+                <FormKit
+                    type="textarea"
+                    label="*How will you use the money?"
+                    validation="required|length:20,500"
+                />
+                </FormKit>
+            </section>
+
+            <details>
+                <summary>Form data</summary>
+                <pre>{{ value }}</pre>
+            </details>
+            </div>
+        </FormKit> -->
+
 
         <div v-if="$page.props.active_session.user">
             <h2>{{ $page.props.active_session.user.name }}</h2>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,13 @@ import '../sass/app.scss'
 
 import '../css/app.css'
 
+import '@formkit/addons/css/multistep';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from 'ziggy-js';
+import { plugin as formkitPlugin, defaultConfig } from '@formkit/vue';
 import { translations } from './Mixins/translations';
 
 
@@ -17,7 +19,8 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         const VueApp = createApp({ render: () => h(App, props) })
         .use(plugin)
-        .use(ZiggyVue, Ziggy);
+        .use(ZiggyVue, Ziggy)
+        .use(formkitPlugin, defaultConfig);
 
         VueApp.mixin(translations);
         VueApp.mount(el);
