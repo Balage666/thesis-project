425eac97067de8548dcc29744fa4658a965515e7
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-11 18:34:05 +0100

    Created Helper class for array of email domains

---
+++ app/Http/Helpers/Shared/Domains.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Helpers\Shared;
+
+class Domains {
+    public static function getStoredDomains() {
+        return ["@gmail.com", "@protonmail.com", "@ncob.com", "@simgatsy.com", "@simgatla.com", "@flipbasket.tp"];
+    }
+}
