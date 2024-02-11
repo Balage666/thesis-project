3dec0b21f38b23785473ddcb0fb586f19f8d2ee6
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-11 18:38:54 +0100

    picked the technologies out of the template
    And put them into an array of objects and exported as a module

---
+++ resources/js/Shared/technologies.js
@@ -0,0 +1,17 @@
+export const usedTechnologies = [
+    { id: Symbol(), url: 'https://laravel.com/', title: 'Laravel', icon: 'fab fa-laravel' },
+    { id: Symbol(), url: 'https://vuejs.org/', title: 'Vue-js', icon: 'fab fa-vuejs' },
+    { id: Symbol(), url: 'https://fontawesome.com/', title: 'Font Awesome', icon: 'fab fa-font-awesome' },
+    { id: Symbol(), url: 'hhttps://getbootstrap.com/', title: 'Bootstrap', icon: 'fab fa-bootstrap' },
+    { id: Symbol(), url: 'https://sass-lang.com/', title: 'Sass', icon: 'fab fa-sass' },
+    { id: Symbol(), url: 'https://developer.mozilla.org/en-US/docs/Web/CSS', title: 'CSS3', icon: 'fab fa-css3-alt' },
+    { id: Symbol(), url: 'https://developer.mozilla.org/en-US/docs/Web/HTML', title: 'HTML', icon: 'fab fa-html5' },
+    { id: Symbol(), url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript', title: 'fab fa-JavaScript', icon: 'fab fa-js' },
+    { id: Symbol(), url: 'https://www.php.net/', title: 'PHP', icon: 'fab fa-php' },
+    { id: Symbol(), url: 'https://vitejs.dev/', title: 'Vite', icon: 'devicon-vitejs-plain' },
+    { id: Symbol(), url: 'https://dev.mysql.com/doc/', title: 'MySQL', icon: 'devicon-mysql-original' },
+    { id: Symbol(), url: 'https://httpd.apache.org/docs/2.4/install.html', title: 'Apache HTTP Server Project', icon: 'devicon-apache-plain' },
+    { id: Symbol(), url: 'https://ubuntu.com/server', title: 'Ubuntu', icon: 'fab fa-ubuntu' },
+    { id: Symbol(), url: 'https://github.com/', title: 'GitHub', icon: 'fab fa-github' },
+    { id: Symbol(), url: 'https://trello.com/en', title: 'Trello', icon: 'fab fa-trello' },
+]
