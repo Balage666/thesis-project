380f86d2d964d9f2d24c90f63a6081354aaf3938
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-19 18:37:19 +0100

    Created Toast component.

---
+++ resources/js/Components/Notification/Toast.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <!-- TODO: Design toast component! -->
+    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <img src="..." class="rounded me-2" alt="...">
+            <strong class="me-auto">Bootstrap</strong>
+            <small class="text-body-secondary">11 mins ago</small>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            Hello, world! This is a toast message.
+        </div>
+    </div>
+</template>
