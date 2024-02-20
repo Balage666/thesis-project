f9705d9acb60fce675bb1145a820fd5d2d4b5803
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-20 16:40:22 +0100

    Created resource for phone and address objects

---
+++ app/Http/Resources/PhoneResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PhoneResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'number' => $this->number,
+            'formatted_number' => $this->formatNumber($this->number, $this->mask),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+
+    private function formatNumber(String $phoneNumber, String $mask) {
+
+        // dd($phoneNumber, $mask);
+
+        $maskParts = explode(' ', $mask);
+        [$countryCode, $realMask] = $maskParts;
+
+
+        for($i = 0; $i< Str::length($phoneNumber); $i++) {
+            $j = 0;
+            while($j < Str::length($realMask) && $realMask[$j] != '9'){
+                $j++;
+            }
+            $realMask[$j] = $phoneNumber[$i];
+        }
+
+        // dd($realMask);
+        return "{$countryCode} {$realMask}";
+    }
+}
