18ceef5ddeb19b53c37d27e23a116802df1b1bb0
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-20 16:42:23 +0100

    Created list and list-item components for phones

---
+++ resources/js/Components/DataDisplay/PhoneNumber/ListPhoneNumbers.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import PhoneNumberItem from '*vue-components/DataDisplay/PhoneNumber/PhoneNumberItem.vue';
+
+const props = defineProps({
+    list: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div class="container">
+        <ul class="list-group" v-if="props.list.length > 0">
+        <!-- <pre v-for="item in props.list">{{ item }}</pre> -->
+            <PhoneNumberItem class="list-group-item" v-for="phoneNumberItem in props.list" :item="phoneNumberItem"/>
+        </ul>
+
+        <h3 v-else class="text-center">There are no available phone numbers!</h3>
+    </div>
+
+</template>
+
---
+++ resources/js/Components/DataDisplay/PhoneNumber/PhoneNumberItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    }
+});
+
+</script>
+
+<template>
+    <li>
+        <div class="row">
+            <div class="col-6 text-start my-4 my-md-2">
+                <span class="fw-bold">
+                    {{ item.formatted_number }}
+                </span>
+            </div>
+            <div class="col-6 text-end">
+                <div class="d-grid d-md-flex align-items-md-end justify-content-md-end gap-2">
+                    <!--TODO: Implement edit and delete phone number methods and routes-->
+                    <button type="button" class="btn btn-primary">Edit</button>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+    </li>
+</template>
+
