b5a594e36a143df23eba722a995e5ff984cf66f2
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-21 16:38:14 +0100

    Created components for address related data ops.

---
+++ resources/js/Components/DataDisplay/Address/ListAddresses.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <h3 class="text-center">There are no addresses!</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ resources/js/Components/Input/CreateAddress.vue
@@ -0,0 +1,83 @@
+<template>
+
+    <!-- <pre>{{ listOfStatesByCountryIso }}</pre> -->
+
+    <form v-if="props.visible" @submit.prevent="sendFormData">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <label class="form-label d-flex justify-content-start" for="country">{{ __('Select country') }}</label>
+                    <select class="form-select form-select-lg formInputFieldBackground" name="country" id="country" @change="getStatesByCountryIso" v-model="addressForm.countryIso">
+                        <option :value="NaN">{{ __('--Select country--') }}</option>
+                        <option v-for="country in listOfCountries" :value="country.isoCode" >{{ __(country.name) }}</option>
+                    </select>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <label class="form-label d-flex justify-content-start" for="region">{{ __("Select your state or region") }}</label>
+                    <!-- <input class="form-control form-control-lg formInputFieldBackground" type="text" name="region" id="region" v-model="addressForm.region"> -->
+                    <select :disabled="listOfStatesByCountryIso.length == 0" :required="listOfStatesByCountryIso.length != 0" class="form-select form-select-lg formInputFieldBackground" name="region" id="region" v-model="addressForm.region">
+                        <option :value="NaN">{{ __('--Select state or region or country--') }}</option>
+                        <option v-for="state in listOfStatesByCountryIso" :value="state.state_code" >{{ __(state.name) }}</option>
+                    </select>
+
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <label class="form-label d-flex justify-content-start" for="postal_zip_code">{{ __("Type your zip or postal code") }}</label>
+                    <input class="form-control form-control-lg formInputFieldBackground" type="text" name="postal_zip_code" id="postal_zip_code" v-model="addressForm.postalZipCode">
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <label class="form-label d-flex justify-content-start" for="address_text">{{ __("Type your address here") }}</label>
+                    <input class="form-control form-control-lg formInputFieldBackground" type="text" name="address_text" id="address_text" v-model="addressForm.addressText">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 d-grid gap-2 mt-4">
+                    <button type="submit" class="btn btn-primary shadow-sm fw-bold">{{ __('Add address') }}</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
+
+import { Country, State, City } from 'country-state-city'
+
+const emits = defineEmits(['submitted']);
+
+const props = defineProps({
+    visible: {
+        type: Boolean,
+        default: false
+    },
+    user: {
+        type: Object,
+        required: true
+    }
+});
+
+const listOfCountries = Country.getAllCountries().map(({ flag, name, isoCode }) => ({ flag, name, isoCode }));
+const listOfStatesByCountryIso = ref([])
+
+const addressForm = useForm({
+    'countryIso': NaN,
+    'region' : NaN,
+    'postalZipCode': '',
+    'addressText': ''
+});
+
+//TODO: Jól van kész lecsukom!
+const getStatesByCountryIso = () => {
+    listOfStatesByCountryIso.value = State.getStatesOfCountry(addressForm.countryIso);
+    // console.log(State.getStatesOfCountry());
+}
+
+const sendFormData = () => {
+    console.log(addressForm);
+}
+
+</script>
