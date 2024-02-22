fa5e8b19196f536145d2db7191f28a8a60621795
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-22 14:27:33 +0100

    Ellaborated Address creating method

--- app/Http/Controllers/Address/AddressController.php
+++ app/Http/Controllers/Address/AddressController.php
@@ -6,18 +6,37 @@ use App\Models\User;
 use App\Models\Address;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Axlon\PostalCodeValidation\Rules\PostalCode;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
     public function Store(Request $request, User $user) {
 
+        // dd($request['countryIso']);
 
         $addressFormFields = $request->validate([
-            // ...
+            'countryIso' => ['required', 'max:2'],
+            'regionIso' => ['required'],
+            'postalZipCode' => ['required'],
+            'addressText' => ['required']
         ]);
 
+        $rules = ['postalZipCode' => PostalCode::for($addressFormFields['countryIso'])];
+
+        $validator = Validator::make($addressFormFields, $rules);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator->errors());
+        }
+
+        // dd($addressFormFields);
+
         Address::create([
-            // ...
+            'country' => $addressFormFields['countryIso'],
+            'postal_or_zip_code' => $addressFormFields['postalZipCode'],
+            'state_or_region' => $addressFormFields['regionIso'],
+            'address_text' => $addressFormFields['addressText'],
             'user_id' => $user->id
         ]);
 
