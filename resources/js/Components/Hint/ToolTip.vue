21a00567e3a43c42ff9bb31d7e6ef3fe066024cd
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-23 15:45:44 +0100

    Renamed PopOver comp. to ToolTip

---
+++ resources/js/Components/Hint/ToolTip.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { Tooltip } from 'bootstrap';
+
+import { onMounted } from 'vue';
+
+const props = defineProps({
+    hint: {
+        type: String,
+        default: 'Default PopOver Hint'
+    }
+})
+
+onMounted(() => {
+    new Tooltip(document.body, {
+        selector: "[data-bs-toggle='tooltip']",
+    })
+})
+
+</script>
+
+<template>
+    <button
+        type="button"
+        class="btn btn-lg btn-outline-warning"
+        data-bs-toggle="tooltip"
+        title="Tooltip title"
+        :data-bs-content="props.hint"
+    >
+        <i class="fa-solid fa-question"></i>
+    </button>
+</template>
+
