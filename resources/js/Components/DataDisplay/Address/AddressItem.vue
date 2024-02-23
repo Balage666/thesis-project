d0edc33aa3e4d45503bdfee581f2413efc944bdf
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-23 15:47:49 +0100

    Created Address listing component

--- resources/js/Components/DataDisplay/Address/AddressItem.vue
+++ resources/js/Components/DataDisplay/Address/AddressItem.vue
@@ -1,9 +1,66 @@
-<template>
-    <div>
+<script setup>
 
+import countries from 'countries-phone-masks';
+
+import { State } from 'country-state-city';
+
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    }
+});
+
+const gatherCorrectFlag = (countryIso) => {
+
+    return countries.find(({ iso }) => iso === countryIso)?.flag;
+
+}
+
+
+const gatherNameOfRegion = (countryIso, regionIso) => {
+
+    return State.getStateByCodeAndCountry(regionIso, countryIso)?.name;
+
+}
+
+</script>
+
+<template>
+
+    <div class="row my-2">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 text-end">
+                                <i class="fa-solid fa-circle-xmark text-danger fw-bold fs-5"></i>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 col-lg-6 my-lg-2 my-0 text-center text-lg-start">
+                                <img :src="gatherCorrectFlag(item.country)" style="width: 50px; height: 50px;" :alt="item.country" :title="item.country">
+                            </div>
+                            <div class="col-12 col-lg-6 text-center text-lg-end">
+                                <div class="col-12 ms-0">
+                                    {{ gatherNameOfRegion(item.country, item.region) ?? '-' }}
+                                </div>
+                                <div class="col-12">
+                                    <p class="fw-bold">
+                                        <span class="badge bg-info">
+                                            {{ item.postal_zip_code }}
+                                        </span>,
+                                        {{ item.address_text }}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup>
-
-</script>
--- resources/js/Components/DataDisplay/Address/ListAddresses.vue
+++ resources/js/Components/DataDisplay/Address/ListAddresses.vue
@@ -1,9 +1,48 @@
+<script setup>
+
+import AddressItem from '*vue-components/DataDisplay/Address/AddressItem.vue';
+
+const props = defineProps({
+    list: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
 <template>
     <div class="container">
-        <h3 class="text-center">There are no addresses!</h3>
+
+        <div v-if="props.list.length > 0">
+
+            <AddressItem v-for="address in props.list" :item="address"/>
+            <!-- <div class="row my-2" v-for="address in props.list">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12 col-lg-6 my-lg-2 my-0 text-center text-lg-start">
+                                        Flag
+                                    </div>
+                                    <div class="col-12 col-lg-6 text-center text-lg-end">
+                                        <div class="col-12 ms-0">
+                                            Region
+                                        </div>
+                                        <div class="col-12">
+                                            Address
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+
+        <h3 v-else class="text-center">There are no addresses!</h3>
     </div>
 </template>
 
-<script setup>
-
-</script>
