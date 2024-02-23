63ee3d1f5499224a3b785a0005fdba4e99a40d36
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-23 15:45:22 +0100

    Tiny bit of desgining the toast comp,

--- resources/js/Components/Notification/Toast.vue
+++ resources/js/Components/Notification/Toast.vue
@@ -1,17 +1,27 @@
 <script setup>
+
+
+
 </script>
 
 <template>
     <!-- TODO: Design toast component! -->
-    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <img src="..." class="rounded me-2" alt="...">
-            <strong class="me-auto">Bootstrap</strong>
-            <small class="text-body-secondary">11 mins ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            Hello, world! This is a toast message.
+    <div class="float-end">
+        <div class="position-fixed fixed-top">
+            <div class="position-relative">
+                <div class="position-absolute top-0 end-0">
+                    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+                            <div class="toast-header">
+                                <strong class="me-auto">Bootstrap</strong>
+                                <small class="text-body-secondary">11 mins ago</small>
+                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                            </div>
+                            <div class="toast-body">
+                                Hello, world! This is a toast message.
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
