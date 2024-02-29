a313b27d9198ecde00dc448ff0d48b2c1231b25f
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-02-29 18:20:17 +0100

    Created new edit mode visibility obj.

---
+++ resources/js/Shared/edit-profile-mode-obj.js
@@ -0,0 +1,10 @@
+export default {
+    changeProfilePictureButtonVisible: false,
+    editNameFormVisible: false,
+    editEmailFormVisible: false,
+    resetPasswordButtonVisible: false,
+    addAddressButtonVisible: false,
+    addPhoneNumberButtonVisible: false,
+    editAddressFormVisible: false,
+    editPhoneNumberFormVisible: false,
+}
--- resources/js/Shared/edit-user-mode-obj.js
+++ resources/js/Shared/edit-user-mode-obj.js
@@ -1,6 +1,17 @@
+import editProfileModeObj from "*js-shared/edit-profile-mode-obj"
+
+const asArray = Object.entries(editProfileModeObj);
+const filtered = asArray.filter(
+    ([key, value]) => !key.toLowerCase().includes('address') && !key.toLowerCase().includes('phone')
+);
+
 export default {
-    changeProfilePictureButtonVisible: false,
-    editNameFormVisible: false,
-    editEmailFormVisible: false,
-    resetPasswordButtonVisible: false,
+    ...Object.fromEntries(filtered)
 }
+
+// export default {
+//     changeProfilePictureButtonVisible: false,
+//     editNameFormVisible: false,
+//     editEmailFormVisible: false,
+//     resetPasswordButtonVisible: false
+// }
