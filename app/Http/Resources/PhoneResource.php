bc8b0e4888041cb28177441c2bd1ddbf740ae088
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-01 16:04:40 +0100

    Added mask attr. for phone objects

--- app/Http/Resources/PhoneResource.php
+++ app/Http/Resources/PhoneResource.php
@@ -18,6 +18,7 @@ class PhoneResource extends JsonResource
         return [
             'id' => $this->id,
             'number' => $this->number,
+            'mask' => $this->mask,
             'formatted_number' => $this->formatNumber($this->number, $this->mask),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
