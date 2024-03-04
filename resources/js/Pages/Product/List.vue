1eaf1df1bb1cae281fe0e30f56b19f9825b14458
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-04 15:54:30 +0100

    Created the base pages for product crud ops

---
+++ resources/js/Pages/Product/List.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+const props = defineProps({
+    products: {
+        type: Object
+    }
+})
+
+</script>
+
+<style scoped>
+
+</style>
+
+<template>
+    <div>
+
+    </div>
+</template>
