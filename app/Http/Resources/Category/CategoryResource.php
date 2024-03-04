0ee50630400054efec4babdd2139599b8c0ed7bc
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-04 15:51:25 +0100

    Created resource object for categories

---
+++ app/Http/Resources/Category/CategoryResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Category;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+
+        // dd($request);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'created_at_human_readable' => $this->created_at->diffForHumans(),
+            'updated_at_human_readable' => $this->updated_at->diffForHumans(),
+        ];
+    }
+}
