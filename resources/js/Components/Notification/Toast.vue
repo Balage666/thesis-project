18c387fcf429a3e10d0f46c26fd5c37532e0b351
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-08 16:46:36 +0100

    Added some functionality to toast comp.

--- resources/js/Components/Notification/Toast.vue
+++ resources/js/Components/Notification/Toast.vue
@@ -1,27 +1,62 @@
 <script setup>
 
+import { onMounted, ref } from 'vue';
 
+const props = defineProps({
+    message: {
+        type: String,
+        default: 'Hello, world! This is a toast message.'
+    },
+    toastType: {
+        type: String,
+        default: 'success'
+    },
+    name: {
+        type: String,
+        default: 'Bootstrap'
+    }
+})
+
+const hidden = ref(false);
+
+const close = () => {
+    hidden.value = true
+}
+
+onMounted(() => {
+    setTimeout(() => {
+        hidden.value = true;
+    }, 2500)
+})
 
 </script>
 
 <template>
     <!-- TODO: Design toast component! -->
-    <div class="float-end">
-        <div class="position-fixed fixed-top">
-            <div class="position-relative">
-                <div class="position-absolute top-0 end-0">
-                    <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <strong class="me-auto">Bootstrap</strong>
-                                <small class="text-body-secondary">11 mins ago</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                Hello, world! This is a toast message.
-                            </div>
-                    </div>
-                </div>
+    <Transition>
+        <div v-show="!hidden" class="toast show my-2" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="me-auto">{{ props.name }}</strong>
+                <!-- <small class="text-body-secondary">11 mins ago</small> -->
+                <button type="button" class="btn-close" @click="close" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{ props.message }}
             </div>
         </div>
-    </div>
+    </Transition>
 </template>
+
+<style scoped>
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+</style>
---
+++ resources/js/Components/Notification/ToastStack.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Toast from '*vue-components/Notification/Toast.vue';
+
+const props = defineProps({
+    notifications: {
+        type: [Array, Object],
+        required: true
+    }
+})
+
+
+
+</script>
+<template>
+    <div class="position-fixed fixed-top">
+        <div class="float-end">
+            <Toast v-for="n in props.notifications" :message="n"/>
+        </div>
+    </div>
+</template>
+
