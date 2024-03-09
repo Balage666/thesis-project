e2d387a8182280e0545c551eba7ba3bb6a72a5c9
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-09 19:39:31 +0100

    Created very basic list page for products

--- resources/js/Pages/Product/List.vue
+++ resources/js/Pages/Product/List.vue
@@ -1,10 +1,16 @@
 <script setup>
 
+import { computed } from 'vue';
+
 const props = defineProps({
     products: {
         type: Object
     }
 })
+
+const productList = computed(() => props.products.data);
+const ObjectProperties = computed(() => Object.getOwnPropertyNames(productList.value[0]).filter(p => p !== 'description').slice(1, 5))
+console.log(ObjectProperties.value);
 
 </script>
 
@@ -13,7 +19,25 @@ const props = defineProps({
 </style>
 
 <template>
+    <!-- <pre>{{ productList }}</pre> -->
     <div>
 
+        <h1>List products (design in development)</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" v-for="property in ObjectProperties">{{ property }}</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr v-for="product in productList">
+                    <!-- <th scope="row">{{ product.id }}</th> -->
+                    <th>{{ product.name }}</th>
+                    <td>{{ product.shortened_description }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.stock }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
