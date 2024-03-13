2f1e13c36a8df5c30384f70701416d30ab9b142c
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-13 21:22:48 +0100

    Created order controller

---
+++ app/Http/Controllers/Order/OrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Order;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function List() {
+
+    }
+
+    public function AllList() {
+
+    }
+
+    public function Create() {}
+
+    public function Store() {}
+
+    public function Show() {
+
+    }
+
+    public function Edit() {
+
+    }
+
+    public function Update() {
+
+    }
+
+    public function Destroy() {
+
+    }
+
+
+}
