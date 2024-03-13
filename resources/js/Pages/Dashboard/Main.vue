db8f56a4f63c5beb867f8756061dd4928bf525ee
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-13 21:28:24 +0100

    Created base page for dashboard

---
+++ resources/js/Pages/Dashboard/Main.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import BodyLayout from '*vue-pages/Layouts/BodyLayout.vue';
+
+</script>
+
+<template>
+    <BodyLayout>
+        <h1>Dashboard - Main</h1>
+    </BodyLayout>
+</template>
+
+
