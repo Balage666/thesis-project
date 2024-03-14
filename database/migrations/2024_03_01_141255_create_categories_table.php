8d5313ddc6d6a69eb9d6da08d9ddba088051c226
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-14 22:25:33 +0100

    Updated migration file of categories table

--- database/migrations/2024_03_01_141255_create_categories_table.php
+++ database/migrations/2024_03_01_141255_create_categories_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id()->unsigned();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
