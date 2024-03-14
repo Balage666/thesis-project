99122532c1fdc17ff633b8023ebb4776313f7d1a
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-14 22:28:46 +0100

    Created show page for single product

--- resources/js/Pages/Product/Show.vue
+++ resources/js/Pages/Product/Show.vue
@@ -1,12 +1,53 @@
 <script setup>
 
+import { usePage, useForm } from '@inertiajs/inertia-vue3';
+import { router } from '@inertiajs/vue3';
+import { ref, computed } from 'vue';
+
+import Galleria from 'primevue/galleria';
+import "primevue/resources/themes/lara-light-cyan/theme.css";
+
 import BodyLayout from '*vue-pages/Layouts/BodyLayout.vue';
 
+const props = defineProps({
+    product: {
+        type: Object
+    },
+    productsInSameCategory: {
+        type: [Array, Object]
+    }
+});
+
+const pageProps = ref(usePage().props.value);
+const currentUser = ref(pageProps.value.active_session.user);
+const permissions = ref(pageProps.value.permissions);
+
+const carouselProducts = computed(() => props.productsInSameCategory.data);
+const productShow = computed(() => props.product.data)
+const productImages = computed(() => productShow.value.images.map((img) => img.product_picture))
+const productComments = computed(() => productShow.value.comments)
+
+const commentForm = useForm({
+    comment: ''
+});
+
+const sendAddCommentRequest = () => {
+
+    console.log(commentForm.comment);
+
+    router.post(route('comment-add', { product: productShow.value }), commentForm)
+
+}
+
+const sendDeleteCommentRequest = (comment) => {
+
+    console.log(comment);
+
+    router.post(route('comment-destroy', { comment: comment }));
+
+}
+
 </script>
-
-<style scoped>
-
-</style>
 
 <template>
 
@@ -15,6 +56,104 @@ import BodyLayout from '*vue-pages/Layouts/BodyLayout.vue';
     </Head>
 
     <BodyLayout>
-        <h1>Show product</h1>
+
+        <pre>{{ permissions }}</pre>
+
+        <div class="card p-5 container-fluid bg-info-subtle border-0">
+
+            <div class="row g-5 d-flex align-items-md-center justify-content-md-center align-items-lg-center justify-content-lg-center">
+                <div class="col-12 col-md-6 col-lg-5">
+
+                    <Galleria :value="productImages" :numVisible="5" containerStyle="max-width: 35rem; border-style:solid; border-width: 5px; border-color: white"
+                        :showThumbnails="false" :showIndicators="true" :changeItemOnIndicatorHover="true">
+                        <template #item="slotProps">
+                            <img :src="slotProps.item" :alt="productShow.name" style="width: 100%; display: block;" />
+                        </template>
+                    </Galleria>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-7">
+                    <h1>{{ productShow.name }}</h1>
+
+                    <div>
+                        <div>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </div>
+                        <span class="text-muted">41 reviews</span>
+                    </div>
+
+                    <p class="lead">{{ productShow.description }}</p>
+
+                    <div class="row">
+
+                        <div class="col-6">
+
+                            <Link :href="route('add-to-basket', { product: productShow })" method="post" as="button" type="button" class="btn btn-lg btn-secondary"> <i class="fa-solid fa-basket-shopping"></i> Add to Basket</Link>
+
+                        </div>
+
+                        <div class="col-6">
+
+                            <Link href="#" method="post" as="button" type="button" class="btn btn-lg btn-outline-danger"> <i class="fa-regular fa-heart"></i> Mark as favorite</Link>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <h3>Comments</h3>
+            </div>
+
+            <hr>
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-12">
+                    <div class="card border">
+                        <div class="card-body p-4">
+                            <div v-if="productComments.length == 0">
+                                <h3>There are no comments!</h3>
+                            </div>
+                            <div v-else class="card mb-4" v-for="comment in productComments">
+                                <div class="card-body">
+                                    <p class="lead">{{ comment.comment }}</p>
+
+                                    <div class="d-flex justify-content-between">
+                                        <div class="d-flex flex-row align-items-center">
+                                            <img class="rounded-circle" :src="comment.commenter.profile_picture" :title="comment.commenter.name" :alt="comment.commenter.name" width="50"
+                                            height="50" />
+                                            <p class="fs-6 mb-0 ms-2">{{ comment.commenter.name }}</p>
+                                        </div>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <button @click="sendDeleteCommentRequest(comment)" v-if="permissions.has_admin_role || currentUser?.id === comment.commenter.id" class="btn btn-lg btn-danger">
+                                                <i class="fa-solid fa-x"></i> Delete
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="form-outline mb-4">
+                                <div class="form-floating">
+                                    <textarea v-model="commentForm.comment" class="form-control" placeholder="Leave a comment here" id="commentInput" style="height: 120px"></textarea>
+                                    <label for="commentInput">{{ __('New comment') }}</label>
+                                </div>
+                            </div>
+                            <div class="form-outline text-end">
+                                <button @click="sendAddCommentRequest" type="submit" class="btn btn-lg btn-secondary">
+                                    <i class="fa-solid fa-right-to-bracket"></i> {{ __('Send') }}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </BodyLayout>
 </template>
