8f73959c1768b287568863a595c539dc9b7e0949
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-14 22:22:17 +0100

    Created methods related to comments

--- app/Http/Controllers/Product/ProductDetailsController.php
+++ app/Http/Controllers/Product/ProductDetailsController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Models\ProductRating;
+use App\Models\ProductComment;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Product\RateProductRequest;
-use App\Models\Product;
-use App\Models\ProductRating;
-use Illuminate\Http\Request;
+use App\Http\Requests\Product\AddProductCommentRequest;
 
 class ProductDetailsController extends Controller
 {
@@ -31,6 +34,34 @@ class ProductDetailsController extends Controller
         ]);
 
         return redirect()->back()->with('message', 'Product rated!');
+
+    }
+
+    public function AddComment(AddProductCommentRequest $request, Product $product) {
+
+        $validated = $request->validated();
+
+        // dd($validated);
+
+        $user = User::find(auth()->user()->id);
+        $newComment = ProductComment::newModelInstance([
+            'comment' => $validated['comment']
+        ]);
+
+        $newComment->CommentedProduct()->associate($product);
+        $newComment->Commenter()->associate($user);
+
+        $newComment->save();
+
+        return redirect()->back()->with('message', 'Comment Added');
+
+    }
+
+    public function DeleteComment(ProductComment $comment) {
+
+        $comment->delete();
+
+        return redirect()->back()->with('message', 'Comment deleted!');
 
     }
 }
