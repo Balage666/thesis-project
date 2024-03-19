02691a1693dff4c4a180f797785b500d3fac6ae1
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-19 18:43:35 +0100

    tested some features

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -54,22 +54,37 @@ const check = (payload) => {
         <BodyLayout>
             <h1>New Feature test</h1>
 
-            <pre>{{ $page.props.permissions }}</pre>
+            <pre>{{ $page.props.guest_cart }}</pre>
 
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">{{ props.products.data[0].name }}</h5>
                     <p class="card-text">{{ props.products.data[0].shortened_description }}</p>
                     <p class="card-text">{{ props.products.data[0].price }}</p>
-                    <p>Rating</p>
-                    <RateProduct :alreadyRated="checkAlreadyRated(currentUser, props.products.data[0])" :product="props.products.data[0]" @onRated="check"/>
+                    <!-- <p>Rating</p>
+                    <RateProduct :alreadyRated="checkAlreadyRated(currentUser, props.products.data[0])" :product="props.products.data[0]" @onRated="check"/> -->
 
                     <!-- <Rating v-model="rating" :stars="5" :cancel="false" @update:modelValue="check"/> -->
-                    <!-- <Link :href="route('add-to-basket', { product: props.products.data[0] })" method="post" as="button" type="button" class="btn btn-primary">Add to Basket</Link>
-                    <Link :href="route('remove-from-basket', { product: props.products.data[0] })" method="post" as="button" type="button" class="btn btn-danger">Remove from Basket</Link> -->
+                    <Link :href="route('add-to-basket', { product: props.products.data[0] })" method="post" as="button" type="button" class="btn btn-primary">Add to Basket</Link>
+                    <Link :href="route('remove-from-basket', { product: props.products.data[0] })" method="post" as="button" type="button" class="btn btn-danger">Remove from Basket</Link>
                 </div>
             </div>
 
+            <div class="card mt-1" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ props.products.data[1].name }}</h5>
+                    <p class="card-text">{{ props.products.data[1].shortened_description }}</p>
+                    <p class="card-text">{{ props.products.data[1].price }}</p>
+                    <!-- <p>Rating</p>
+                    <RateProduct :alreadyRated="checkAlreadyRated(currentUser, props.products.data[1])" :product="props.products.data[1]" @onRated="check"/> -->
+
+                    <!-- <Rating v-model="rating" :stars="5" :cancel="false" @update:modelValue="check"/> -->
+                    <Link :href="route('add-to-basket', { product: props.products.data[1] })" method="post" as="button" type="button" class="btn btn-primary">Add to Basket</Link>
+                    <Link :href="route('remove-from-basket', { product: props.products.data[1] })" method="post" as="button" type="button" class="btn btn-danger">Remove from Basket</Link>
+                </div>
+            </div>
+
+            <Link :href="route('clear-basket')" method="post" as="button" type="button" class="mt-1 btn btn-danger">Clear Basket</Link>
 
             <!-- <LanguageSwitcher/> -->
         </BodyLayout>
