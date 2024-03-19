643226c0a8d377e482f072f55ef6e82c953be6b1
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-19 18:41:10 +0100

    created base of charts page

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Models\User;
 use Inertia\Inertia;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Category\CategoryResource;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\User\UserResource;
+use App\Models\Product;
 
 class DashboardController extends Controller
 {
     public function Main() {
 
         return Inertia::render("Dashboard/Main");
+
+    }
+
+    public function Charts() {
+
+        $allUsers = User::all();
+        $allCategories = Category::all();
+        $allProducts = Product::all();
+
+        return Inertia::render("Dashboard/Chart", [
+            'allUsers' => UserResource::collection($allUsers),
+            'allCategories' => CategoryResource::collection($allCategories),
+            'allProducts' => ProductResource::collection($allProducts)
+        ]);
 
     }
 }
