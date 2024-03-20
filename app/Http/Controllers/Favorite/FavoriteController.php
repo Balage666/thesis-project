d49e594d5726ab7adbc84a816f807984b9070bc5
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-20 20:22:03 +0100

    Implemented add and remove methods for favorites

---
+++ app/Http/Controllers/Favorite/FavoriteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Favorite;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Favorite;
+
+class FavoriteController extends Controller
+{
+    public function AddToFavorites(User $user, Product $product) {
+
+        $newFavoriteProduct = Favorite::newModelInstance([]);
+
+        $newFavoriteProduct->Product()->associate($product);
+
+        $user->Favorites()->save($newFavoriteProduct);
+
+        return redirect()->back()->with('message', 'Added to Favorites!');
+    }
+
+    public function RemoveFromFavorites(User $user, Product $product) {
+
+        $foundProduct = $user->Favorites()->whereHas('Product', function ($query) use($product) {
+            $query->where('id', '=', $product->id);
+        })->first()->Product;
+
+        // dd(is_null($foundProduct));
+
+        if (is_null($foundProduct)) {
+            return redirect()->back()->withErrors(['favorite' => 'This product is not in your favorites yet!']);
+        }
+
+        $user->Favorites()->whereHas('Product', function ($query) use($product) {
+            $query->where('id', '=', $product->id);
+        })->first()->delete();
+
+
+        return redirect()->back()->with('message', 'Product removed from favorites!');
+
+    }
+}
