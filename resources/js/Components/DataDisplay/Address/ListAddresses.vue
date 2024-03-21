81a928579270a64cb38019109fdfa1a0a99d6760
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-21 18:28:45 +0100

    Deleted comments, added trans. method

--- resources/js/Components/DataDisplay/Address/ListAddresses.vue
+++ resources/js/Components/DataDisplay/Address/ListAddresses.vue
@@ -17,32 +17,9 @@ const props = defineProps({
         <div v-if="props.list.length > 0">
 
             <AddressItem v-for="address in props.list" :item="address"/>
-            <!-- <div class="row my-2" v-for="address in props.list">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-12 col-lg-6 my-lg-2 my-0 text-center text-lg-start">
-                                        Flag
-                                    </div>
-                                    <div class="col-12 col-lg-6 text-center text-lg-end">
-                                        <div class="col-12 ms-0">
-                                            Region
-                                        </div>
-                                        <div class="col-12">
-                                            Address
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
         </div>
 
-        <h3 v-else class="text-center">There are no addresses!</h3>
+        <h3 v-else class="text-center">{{ __('There are no addresses!') }}</h3>
     </div>
 </template>
 
