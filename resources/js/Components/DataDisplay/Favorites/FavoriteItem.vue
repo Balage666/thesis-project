205a26216fec9df7a35fe3fbfaf57fc269247453
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-21 18:27:03 +0100

    Created components for favorite items

---
+++ resources/js/Components/DataDisplay/Favorites/FavoriteItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    }
+});
+
+const emits = defineEmits(['onItemDeleteRequest']);
+
+const emitFavoriteDeleteRequest = (product) => {
+
+    emits('onItemDeleteRequest', product);
+
+}
+
+</script>
+<template>
+    <li>
+        <div class="row">
+            <div class="col-6 text-start my-4 my-md-2">
+                <div class="row d-flex align-items-center justify-content-between">
+                    <div class="col-6">
+                        <img :src="item.product.preview_image" :alt="item.product.name" width="50px" height="50px">
+                    </div>
+                    <div class="col-6">
+                        <span class="fw-bold">{{ item.product.name }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 text-end d-flex align-items-center justify-content-evenly">
+
+                <button @click="emitFavoriteDeleteRequest(item.product)" type="button" class="btn btn-danger">{{ __('Delete') }}</button>
+
+            </div>
+
+        </div>
+    </li>
+</template>
---
+++ resources/js/Components/DataDisplay/Favorites/ListFavorites.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import FavoriteItem from '*vue-components/DataDisplay/Favorites/FavoriteItem.vue';
+
+const props = defineProps({
+    list: {
+        type: Array,
+        required: true
+    }
+})
+
+const emits = defineEmits(['listChanged'])
+
+const sendListChanged = (payload) => {
+
+    emits('listChanged', payload);
+}
+
+</script>
+
+<template>
+    <div class="container">
+
+        <ul class="list-group" v-if="props.list.length > 0">
+
+            <FavoriteItem
+                class="list-group-item"
+                v-for="favorite in props.list"
+                :item="favorite"
+                @onItemDeleteRequest="sendListChanged"
+                />
+
+        </ul>
+
+        <h3 v-else class="text-center">{{ __('There are no favorite products!') }}</h3>
+
+    </div>
+</template>
+
