aac772436f9c26e3209821452c2efa0a76649c70
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-22 21:25:05 +0100

    Designed filters, added functionality
    Fixed problem related to axios requests not working correctly locales other than english... Hope it won't emerge again...

--- resources/js/Pages/Storefront.vue
+++ resources/js/Pages/Storefront.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3';
-import { usePage } from '@inertiajs/inertia-vue3';
+import { usePage, useForm } from '@inertiajs/inertia-vue3';
 import { computed, ref } from 'vue';
 
 import { useIntersectionObserver } from '@vueuse/core';
@@ -11,6 +11,9 @@ import "primevue/resources/themes/lara-light-cyan/theme.css";
 import BodyLayout from '*vue-pages/Layouts/BodyLayout.vue';
 import ProductCarousel from '*vue-components/DataDisplay/Product/ProductCarousel.vue';
 import Pagination from '*vue-components/DataDisplay/Pagination.vue';
+
+import { translations } from '../Mixins/translations'
+
 import axios from 'axios';
 import { router } from '@inertiajs/vue3';
 
@@ -20,6 +23,12 @@ const props = defineProps({
     },
     allProducts: {
         type: [Array, Object]
+    },
+    categories: {
+        type: [Array, Object]
+    },
+    distributors: {
+        type: [Array, Object]
     }
 })
 
@@ -27,8 +36,45 @@ const pageProps = ref(usePage().props.value);
 const permissions = ref(pageProps.value.permissions);
 
 const currentUser = ref(pageProps.value.active_session.user);
+const currentLocale = ref(pageProps.value.current_locale);
+const filterForm = useForm({
 
-const rangeInput = ref([]);
+    selectedCategories: route().params.selectedCategories || [],
+    selectedDistributors: route().params.selectedDistributors || [],
+    rangeInput: route().params.rangeInput || [],
+    availability: route().params.availability || false
+});
+
+const selectedFilters = computed(() => {
+    // console.log(filterForm.selectedDistributors.length);
+    let count = filterForm.selectedCategories.length + filterForm.selectedDistributors.length + (filterForm.rangeInput.length == 2 ? 1 : 0) + (filterForm.availability ? 1 : 0);
+
+    // console.log(count);
+
+    return count;
+
+})
+
+const filter = () => {
+
+    console.log(filterForm);
+    const {selectedCategories, selectedDistributors, availability, rangeInput} = filterForm;
+    router.get(route(route().current()), { selectedCategories: selectedCategories, selectedDistributors: selectedDistributors, rangeInput: rangeInput, availability: availability })
+}
+
+const clearFilters = () => {
+
+    filterForm.selectedCategories = [];
+    filterForm.selectedDistributors = [];
+    filterForm.rangeInput = [];
+    filterForm.availability = false;
+
+    console.log(route().current());
+    router.get(route(route().current()));
+
+}
+
+// console.log(filterForm.availability);
 
 const carouselProductsShowCase = computed(() => props.carouselProducts.data);
 const allProductsShowCase = computed(() => props.allProducts.data);
@@ -40,12 +86,11 @@ const calculateStars = (avg) => {
 
 const scrollable = ref(null);
 
-const next = ref(props.allProducts.meta.links.filter(l => l.label === 'Next').shift().url);
-
+console.log(currentLocale.value);
+const next = ref(currentLocale.value == 'en' ? props.allProducts.meta.links.filter(l => l.label == 'Next').shift()?.url : props.allProducts.meta.links.filter(l => l.label == 'Következő').shift()?.url);
+console.log(next.value);
 
 useIntersectionObserver(scrollable, ([{ isIntersecting }]) => {
-    // console.log(isIntersecting);
-
     if (!isIntersecting) {
         return
     }
@@ -57,15 +102,11 @@ useIntersectionObserver(scrollable, ([{ isIntersecting }]) => {
 
         axios.get(`${next.value}`).then((response) => {
 
-            console.log(response.data.meta);
-            // console.log(response.data.data);
-
 
             props.allProducts.data.push(...response.data.data);
             console.log(props.allProducts.data, props.allProducts.meta, response.data.data, response.data.meta);
             props.allProducts.meta = response.data.meta;
-
-            next.value = props.allProducts.meta.links.filter(l => l.label === 'Next').shift().url;
+            next.value = currentLocale.value == 'en' ? props.allProducts.meta.links.filter(l => l.label == 'Next').shift()?.url : props.allProducts.meta.links.filter(l => l.label == 'Következő').shift()?.url;
 
         })
     }
@@ -85,8 +126,6 @@ const processEmit = (payload) => {
 
 const addToFavorites = (user, product) => {
 
-
-    // console.log(user);
     router.post(route('add-to-favorites', { user: user, product: product }));
 
 }
@@ -111,8 +150,6 @@ const addToCart = (product) => {
 
     <div>
 
-        <!-- <pre>{{ currentUser.cart }}</pre> -->
-        <!-- <pre>{{ pageProps.guest_cart }}</pre> -->
         <BodyLayout>
 
             <div class="container-fluid bg-info-subtle border-0">
@@ -130,7 +167,7 @@ const addToCart = (product) => {
                             class="d-none d-md-none d-lg-flex"
                             :carouselId="'large'"
                             :products="carouselProductsShowCase"
-                            :visiblePerSlide="3"
+                            :visiblePerSlide="4"
                             @favoriteAdded="processEmit"
                             @favoriteRemoved="processEmit"
                         />
@@ -160,26 +197,31 @@ const addToCart = (product) => {
                     <div class="col-12 col-md-4 filter-board-bg-color border-0 rounded-top-5 rounded-bottom-0 p-3">
 
                         <div class="text-center">
-                            <h3>Filters</h3>
+                            <h3>{{ __('Filters') }}</h3>
                         </div>
                         <hr>
                         <div class="container">
 
-                            <form>
+                            <form @submit.prevent="filter">
                                 <div class="row">
                                     <div class="col-12">
-                                        <h4>Categories</h4>
+                                        <h4>{{ __('Categories') }}</h4>
                                         <hr>
                                     </div>
 
-                                    <div class="col-12 form-check-inline d-flex gap-2">
-                                        <label class="form-check-label" for="category1">Category 1</label>
-                                        <input class="form-check-input" type="checkbox" name="category1" id="category1">
-                                    </div>
-
-                                    <div class="col-12 form-check-inline d-flex gap-2">
-                                        <label class="form-check-label" for="category2">Category 2</label>
-                                        <input class="form-check-input" type="checkbox" name="category2" id="category2">
+                                    <div class="overflow-y-scroll overflow-x-hidden" style="height: 300px">
+                                        <div class="col-12 form-check-inline d-flex gap-2" v-for="category in props.categories">
+                                            <label class="form-check-label" :for="category.id">{{ __(category.name) }}</label>
+                                            <input
+                                                class="form-check-input"
+                                                type="checkbox"
+                                                :name="category.name"
+                                                :id="category.id"
+                                                :value="category.id"
+                                                v-model="filterForm.selectedCategories"
+                                                :true-value="[]"
+                                            >
+                                        </div>
                                     </div>
 
                                 </div>
@@ -187,18 +229,23 @@ const addToCart = (product) => {
                                 <div class="row mt-3">
 
                                     <div class="col-12">
-                                        <h4>Distributors</h4>
+                                        <h4>{{ __('Distributors') }}</h4>
                                         <hr>
                                     </div>
 
-                                    <div class="col-12 form-check-inline d-flex gap-2">
-                                        <label class="form-check-label" for="distributor1">Distributor 1</label>
-                                        <input class="form-check-input" type="checkbox" name="distributor1" id="distributor1">
-                                    </div>
-
-                                    <div class="col-12 form-check-inline d-flex gap-2">
-                                        <label class="form-check-label" for="distributor2">Distributor 2</label>
-                                        <input class="form-check-input" type="checkbox" name="distributor2" id="distributor2">
+                                    <div class="overflow-y-scroll overflow-x-hidden" style="height: 200px">
+                                        <div class="col-12 form-check-inline d-flex gap-2" v-for="distributor in props.distributors">
+                                            <label class="form-check-label" :for="distributor.id">{{ distributor.name }}</label>
+                                            <input
+                                                class="form-check-input"
+                                                type="checkbox"
+                                                :name="distributor.name"
+                                                :id="distributor.id"
+                                                :value="distributor.id"
+                                                v-model="filterForm.selectedDistributors"
+                                                :true-value="[]"
+                                            >
+                                        </div>
                                     </div>
 
                                 </div>
@@ -206,32 +253,50 @@ const addToCart = (product) => {
                                 <div class="row mt-3">
 
                                     <div class="col-12">
-                                        <h4>Price</h4>
+                                        <h4>{{ __('Price') }}</h4>
                                         <hr>
                                     </div>
 
-                                    <div class="col-12 mt-1">
-                                        <Slider :min="0" :max="250000" v-model="rangeInput" range/>
+                                    <div class="col-12">
+                                        <div class="row d-flex align-content-between gap-2">
+
+                                            <div class="col-12 col-md-2 ps-0 pe-0 text-center">
+                                                <input type="number" style="width: 50px" v-model="filterForm.rangeInput[0]" readonly>
+                                            </div>
+
+                                            <div class="col-12 col-md-7 px-0 text-center ">
+                                                <Slider :min="1" :max="100" v-model="filterForm.rangeInput" range/>
+                                            </div>
+
+                                            <div class="col-12 col-md-2 ps-0 pe-0 text-center">
+                                                <input type="number" style="width: 50px" v-model="filterForm.rangeInput[1]" readonly>
+                                            </div>
+                                        </div>
+
                                     </div>
 
-                                    <div class="col-6">
-                                        <input type="number" v-model="rangeInput[0]" disabled>
-                                    </div>
-                                    <div class="col-6">
-                                        <input type="number" v-model="rangeInput[1]" disabled>
-                                    </div>
 
                                 </div>
 
                                 <div class="row mt-3">
                                     <div class="col-12">
-                                        <h4>Availability</h4>
+                                        <h4>{{ __('Availability') }}</h4>
                                         <hr>
                                     </div>
 
                                     <div class="col-12 form-check-inline d-flex gap-2">
-                                        <label class="form-check-label" for="stock">In Stock</label>
-                                        <input class="form-check-input" type="checkbox" name="stock" id="stock">
+                                        <label class="form-check-label" for="stock">{{ __('In Stock') }}</label>
+                                        <input class="form-check-input" type="checkbox" name="stock" id="stock" v-model="filterForm.availability">
+                                    </div>
+                                </div>
+
+                                <div class="row mt-3 text-center d-flex align-items-center">
+                                    <div class="col-12 col-md-6">
+                                        <button type="submit" class="btn btn-lg btn-info">{{ __('Filter') }}</button>
+                                    </div>
+
+                                    <div class="col-12 col-md-6 mt-sm-2">
+                                        <button type="button" @click="clearFilters" class="btn btn-lg btn-secondary">{{ __('Clear') }} <span v-if="selectedFilters > 0">{{ selectedFilters }}</span></button>
                                     </div>
                                 </div>
                             </form>
@@ -243,16 +308,18 @@ const addToCart = (product) => {
                     <div class="col-12 col-md-8 p-3">
 
                         <div class="text-center">
-                            <h3>Products</h3>
+                            <h3>{{ __('Products') }}</h3>
                         </div>
 
                         <hr>
 
                         <div class="container">
 
-                            <div class="row overflow-y-scroll" style="height: 650px">
+                            <div class="row overflow-y-scroll" style="height: 950px">
 
-                                <div class="mt-2 col-md-12 col-lg-4 mb-4 mb-lg-0" v-for="product in props.allProducts.data" :key="product.id">
+                                <h4 v-if="props.allProducts.data.length == 0">No products found!</h4>
+
+                                <div v-else class="mt-2 col-md-12 col-lg-4 mb-4 mb-lg-0" v-for="product in props.allProducts.data" :key="product.id">
 
                                     <div class="card border-3 border-info">
                                         <img :src="product.preview_image" class="card-img-top" :title="product.category.name" :alt="product.name" style="height: 260px;" />
@@ -269,7 +336,7 @@ const addToCart = (product) => {
                                             </div>
 
                                             <div class="d-flex justify-content-between mb-2">
-                                                <p class="text-muted mb-0">Available: <span class="fw-bold">{{ product.stock }}</span></p>
+                                                <p class="text-muted mb-0">{{ __('Available:') }} <span class="fw-bold">{{ product.stock }}</span></p>
                                                 <div class="ms-auto text-warning">
                                                     <i v-for="s in calculateStars(product.avg_of_ratings)" class="fa fa-star"></i>
                                                 </div>
@@ -290,7 +357,6 @@ const addToCart = (product) => {
 
                                 <div v-if="next !== null" class="mt-2 col-md-12 col-lg-4 mb-4 mb-lg-0" ref="scrollable" v-for="n in 3">
                                     <div class="card" aria-hidden="true">
-                                        <!-- <img src="x" class="card-img-top" alt="..."> -->
                                         <div class="card-body">
                                             <h5 class="card-title placeholder-glow">
                                                 <span class="placeholder col-6"></span>
