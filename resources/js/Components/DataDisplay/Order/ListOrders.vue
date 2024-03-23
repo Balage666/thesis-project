96e54158739dfc26cd4f9a1fa0c342f90d83c196
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-23 20:08:03 +0100

    Created components for orders in user show page

---
+++ resources/js/Components/DataDisplay/Order/ListOrders.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import OrderItem from '*vue-components/DataDisplay/Order/OrderItem.vue';
+
+const props = defineProps({
+    list: {
+        type: [Array, Object],
+        required: true,
+    }
+})
+
+</script>
+<template>
+    <div class="container">
+        <ul class="list-group" v-if="props.list.length > 0">
+            <OrderItem class="list-group-item" v-for="orderItem in props.list" :item="orderItem" />
+        </ul>
+
+        <h3 v-else class="text-center">{{ __('No orders found!') }}</h3>
+    </div>
+</template>
+
---
+++ resources/js/Components/DataDisplay/Order/OrderItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import { router } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/inertia-vue3';
+
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    }
+})
+
+const sendDeleteOrderRequest = () => {
+
+    router.post(route('order-destroy', { order: props.item }));
+
+}
+
+</script>
+<template>
+    <li>
+        <div class="row">
+            <div class="col-6 text-start my-4 my-md-2">
+                <div class="row d-flex align-items-center justify-content-between">
+                    <div class="col-6">
+                        <Link :href="route('order-show', { order: props.item })" class=" text-black text-decoration-none ">
+                            <span><i class="fa-solid fa-boxes-packing"></i> {{ `${__('Order #')}${item.id}` }}</span>
+                        </Link>
+                    </div>
+                    <div class="col-6">
+                        <span class="fw-bold">{{ item.status }}</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-6 text-end d-flex align-items-center justify-content-evenly">
+
+                <button @click="sendDeleteOrderRequest" type="button" class="btn btn-danger">{{ __('Delete') }}</button>
+
+            </div>
+
+        </div>
+    </li>
+</template>
+
