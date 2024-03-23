41a2277911823cc46417e370ca8835376106f8f4
Balazs Karacsony <karacsonybalazs01@gmail.com>
2024-03-23 20:09:20 +0100

    Changed visibility of a certain option on main

--- resources/js/Pages/Dashboard/Main.vue
+++ resources/js/Pages/Dashboard/Main.vue
@@ -60,8 +60,8 @@ console.log(permissions.value);
                     </Link>
                 </div>
 
-                <div class="col">
-                    <h2 class="mt-3 mb-3">{{ __('Order list') }}</h2>
+                <div class="col" v-if="permissions.has_admin_role">
+                    <h2 class="mt-3 mb-3">{{ __('All Orders list') }}</h2>
                     <Link :href="route('all-orders')" class="btn btn-lg btn-warning" type="button" as="button">
                         <div class="p-5">
                             <i class="fa-solid fa-boxes-stacked fa-2xl"></i>
